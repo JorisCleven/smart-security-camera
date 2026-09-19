bb12c158a8c3cd0201d7f8dea3dd2cf29331777c
Joris Cleven <joriscleven@hotmail.nl>
2026-09-19 14:07:07 +0200

    add webcam capture

--- main.py
+++ main.py
@@ -1,1 +1,24 @@
-print("Smart Security Gestart!")
+import cv2
+
+camera = cv2.VideoCapture(0)
+
+if not camera.isOpened():
+    raise RuntimeError("Camera niet beschikbaar")
+   
+
+while True:
+    success, frame = camera.read()
+
+    if not success:
+        print("Kon geen frame lezen")
+        break
+
+    cv2.imshow("Smart Security Camera", frame)
+
+    key = cv2.waitKey(1)
+
+    if key == ord("q"):
+        break
+
+camera.release()
+cv2.destroyAllWindows()
